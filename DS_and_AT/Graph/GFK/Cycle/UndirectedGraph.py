df87f90dc5a5b53aa3dca5b4af652ab84dcab1bc
Gan3i <Gani468@gmail.com>
2020-08-08 01:49:55 +0530

    cycle detection in undirected graph with union- find/ merge find/disjoint set

---
+++ DS_and_AT/Graph/GFK/Cycle/UndirectedGraph.py
@@ -0,0 +1,51 @@
+from collections import defaultdict
+
+
+class Graph():
+    def __init__(self,vertices):
+        self.vertices = vertices
+        self.graph = defaultdict(list)
+
+    def addEdge(self, u,v):
+        self.graph[u].append(v)
+
+    def isCyclic(self):
+
+        subset = [-1] * self.vertices
+
+        for i in self.graph:
+            for j in self.graph[i]:
+                x = self.find_parent(subset, i)
+                y = self.find_parent(subset, j)
+                if x== y :
+                    return True
+                subset[x] = y 
+
+        return False
+    def find_parent(self, subset,i):
+        if subset[i] == -1:
+            return i
+        return self.find_parent(subset, subset[i])
+
+    def union(self,subset,x, y):
+        # x_set = self.find_parent(subset, x)
+        # y_set = self.find_parent(subset, y)
+        subset[x] = y 
+
+
+g = Graph(10)
+
+g.addEdge(0,1)
+# g.addEdge(1,0)
+g.addEdge(0,2)
+g.addEdge(0,3)
+g.addEdge(0,4)
+# g.addEdge(0,5)
+g.addEdge(2,6)
+g.addEdge(2,5)
+g.addEdge(3,7)
+# g.addEdge(3,1)
+g.addEdge(7,8)
+g.addEdge(7,9)
+
+print(g.isCyclic())
