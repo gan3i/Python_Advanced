bf871477b8cfa509ec8406b9d61f602cfee91ec5
Gan3i <Gani468@gmail.com>
2020-08-30 10:17:38 +0530

    bubble sort optimization

--- DS_and_AT/Sorting/BubbleSort.py
+++ DS_and_AT/Sorting/BubbleSort.py
@@ -1,11 +1,22 @@
 nums = [4,1,5,2,3,6,6,6]
+nums = [1,2,3,4,5]
 
 
 def bubble_sort(nums):
     ln = len(nums)
+    swap = True
     for _ in range(ln):
+        if not swap:
+            return
+        swap = False
+        print("hello")
         for j in range(ln-1):
+
             if nums[j]>nums[j+1]:
                 nums[j],nums[j+1] = nums[j+1],nums[j]
+                swap = True
+
+
+
 bubble_sort(nums)
 print(nums)
