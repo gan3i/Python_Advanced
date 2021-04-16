dccdec5a1b7686bd58ec0a45f25c9234972ae402
Gan3i <Gani468@gmail.com>
2021-04-16 09:49:19 +0530

    stacks demo

--- DS_and_AT/Stack/stacks_demo.py
+++ DS_and_AT/Stack/stacks_demo.py
@@ -1,6 +1,6 @@
 
 stack1 = list()
-stack1.append(0)
+stack1.append(0)    
 stack1.pop() 
 stack1[-1]
 
@@ -11,20 +11,13 @@ stack2.append(0)
 stack2.pop()
 stack2.appendleft(1)
 stack2.popleft()
+print(stack2.count(0))
 
-a = [286]
-a_id = id(a[0])
-i = 286
-while True:
-    print(id(a[-1]))
-    i +=1
-    a.append(i)
-    if id(a[0])  != a_id:
-        break
-    if len(a) == 100:
-        break
+from queue import LifoQueue
 
-print(len(a))
+stack3 = LifoQueue()
+stack3.put(13)
+stack3.get()
 
 
 
